49e0e557ea12db0e1f7e6e311ecc8eef653a497e
ALISSON FERREIRA <alisson_ferreira152@estudante.sesisenai.org.br>
2026-04-01 21:50:57 -0300

    pages

---
+++ novoProjeto - Copia/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './pages/Home';
+import Elite from './pages/Elite';
+import Fileh from './pages/Fileh';
+
+
+function App() {
+const[tela, setTela] = useState(<Home/>)
+
+  return (
+    <div className="cont-app">
+    <header className={"cont-header"}>
+     <nav>
+     <button className={'botoes-nav'} onClick={()=> setTela(<Home/>)}>Home</button>
+     <button className={'botoes-nav'} onClick={()=> setTela(<Fileh/>)}>Fileh</button>
+     <button className={'botoes-nav'} onClick={()=> setTela(<Elite/>)}>Elite</button>
+    </nav>
+      </header>
+    <main className={"cont-main"}>{tela}
+    <Home/>
+    <Elite/>
+     <Fileh /> 
+     </main>
+    </div>
+  )
+}
+
+export default App
+
---
+++ novoProjeto - Copia/src/pages/Fileh.jsx
@@ -0,0 +1,11 @@
+
+
+function Fileh() {
+  return (
+    <div className={'cont-fileh'}>
+<h1>Fileh</h1>
+    </div>
+  )
+}
+
+export default Fileh
